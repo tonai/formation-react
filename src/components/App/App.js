107e434e0a6792669d9e23d892eaf79d02003afd
Tony CABAYE <tony.cabaye@smile.fr>
2020-12-16 12:00:36 +0100

    Create Article component

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import Article from "../Article/Article";
+
+function App() {
+  return (
+    <div className="App">
+      Hello world 2
+      <Article />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Article/Article.js
@@ -0,0 +1,11 @@
+function Article() {
+  const title = 'Article 42';
+
+  return (
+    <div>
+      {title}
+    </div>
+  );
+}
+
+export default Article;
