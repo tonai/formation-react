2fb200b0e16af9c54a3ab6afc78fa9de9e120cbf
Tony CABAYE <tony.cabaye@smile.fr>
2020-12-16 16:33:33 +0100

    Add isSelected state.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,18 +4,35 @@ import Article from "../Article/Article";
 import Header from "../Header/Header";
 
 function App() {
-  const art = {
-    id: 0,
-    title: 'Article 1',
-    published: false,
-    category: 'News',
-    content: 'Lorem ipsum'
-  };
+  const articles = [
+    {
+      id: 0,
+      title: 'Article 1',
+      published: false,
+      category: 'News',
+      content: 'Lorem ipsum'
+    },
+    {
+      id: 1,
+      title: 'Article 2',
+      published: true,
+      category: 'News',
+      content: 'Lorem ipsum'
+    },
+    {
+      id: 2,
+      title: 'Article 3',
+      published: true,
+      category: 'Blog',
+      content: 'Lorem ipsum'
+    }
+  ];
+  const list = articles.map(article => <Article article={article} key={article.id} />);
 
   return (
     <div className="App">
       <Header alt="logo" />
-      <Article article={art} />
+      {list}
     </div>
   );
 }
--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,15 +1,28 @@
+import { useState } from 'react';
 import './Article.css';
 
 function Article(props) {
+  const [isSelected, setIsSelected] = useState(false);
+  // destructuring shortcut equivalent to:
+  // const state = useState(false);
+  // const isSelected = state[0];
+  // const setIsSelected = state[1];
+
   const rootClassName = [
-    props.article.published ?
-      'visible' :
-      'hidden',
+    props.article.published ? 'visible' : 'hidden',
+    isSelected ? 'selected' : '',
     'article'
   ];
 
+  function handleClick() {
+    setIsSelected(!isSelected);
+  }
+
   return (
-    <div className={rootClassName.join(' ')}>
+    <div
+      className={rootClassName.join(' ')}
+      onClick={handleClick}
+    >
       <div>{props.article.title}</div>
       <div>{props.article.category}</div>
       <div>{props.article.published ?
