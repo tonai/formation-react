5e177edd923e0899217b4a6774197b0d019c09ba
Tony CABAYE <tony.cabaye@smile.fr>
2021-01-28 11:17:24 +0100

    add Filters component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,24 +6,48 @@ import useArticles from '../../hooks/useArticles';
 
 import Article from "../Article/Article";
 import Cart from "../Cart/Cart";
+import Filters from "../Filters/Filters"
 import Header from "../Header/Header";
 import Resize from "../Resize/Resize";
 import Title from "../Title/Title";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
+  const [published, setPublished] = useState('');
   const articles = useArticles();
 
   function handleClick() {
     setCounter(counter + 1);
   }
 
-  const list = articles.map(article => <Article article={article} key={article.id} />);
+  const filteredArticles = articles.filter(article => {
+    return article.title.includes(title);
+  }).filter(article => {
+    return category === '' || article.category === Number(category);
+  }).filter(article => {
+    return published === '' ||
+      (article.published === true && published === 'published') ||
+      (article.published === false && published === 'draft');
+  });
+
+  const list = filteredArticles.map(article =>
+    <Article article={article} key={article.id} />
+  );
 
   return (
     <div className="App">
       <Header alt="logo" />
       <Title title="Homepage" />
+      <Filters
+        title={title}
+        setTitle={setTitle}
+        category={category}
+        setCategory={setCategory}
+        published={published}
+        setPublished={setPublished}
+      />
       {list}
       <Cart />
       <Resize />
---
+++ src/components/Filters/Filters.js
@@ -0,0 +1,55 @@
+function Filters(props) {
+
+  function handleTitleChange(event) {
+    props.setTitle(event.target.value);
+  }
+
+  function handleCategoryChange(event) {
+    props.setCategory(event.target.value);
+  }
+
+  function handlePublishedChange(event) {
+    props.setPublished(event.target.value);
+  }
+
+  return (
+    <div>
+      <input onChange={handleTitleChange} value={props.title}/>
+      <select onChange={handleCategoryChange} value={props.category}>
+        <option value=""></option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+      </select>
+
+      <label htmlFor="all">All:</label>
+      <input
+        type="radio"
+        value=""
+        name="published"
+        id="all"
+        onChange={handlePublishedChange}
+        checked={props.published === ''}
+      />
+      <label htmlFor="published">Published:</label>
+      <input
+        type="radio"
+        value="published"
+        name="published"
+        id="published"
+        onChange={handlePublishedChange}
+        checked={props.published === 'published'}
+      />
+      <label htmlFor="draft">Draft:</label>
+      <input
+        type="radio"
+        value="draft"
+        name="published"
+        id="draft"
+        onChange={handlePublishedChange}
+        checked={props.published === 'draft'}
+      />
+    </div>
+  );
+}
+
+export default Filters;
