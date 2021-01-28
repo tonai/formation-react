4074687180b90b19a0a9f860b9bff5a3e344569a
Tony CABAYE <tony.cabaye@smile.fr>
2021-01-28 15:55:59 +0100

    add create article form

--- src/components/ArticlePage/ArticlePage.js
+++ src/components/ArticlePage/ArticlePage.js
@@ -1,9 +1,35 @@
+import { useState } from 'react';
+
+import { createArticle } from '../../services/article';
+
 import Title from '../Title/Title';
 
 function ArticlePage() {
+  const [title, setTitle] = useState('');
+
+  function handleChange(event) {
+    setTitle(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    createArticle({
+      title: title,
+      category: 1,
+      published: false
+    });
+  }
+
   return (
     <div>
       <Title title="Create new article" />
+      <form onSubmit={handleSubmit}>
+        <label>
+          Title:
+          <input onChange={handleChange} value={title} />
+        </label>
+        <input type="submit" />
+      </form>
     </div>
   );
 }
--- src/services/article.js
+++ src/services/article.js
@@ -2,3 +2,13 @@ export function getArticles() {
   return fetch('http://localhost:3001/articles')
     .then(response => response.json());
 }
+
+export function createArticle(article) {
+  return fetch('http://localhost:3001/articles', {
+    body: JSON.stringify(article),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'POST'
+  }).then(data => data.json());
+}
